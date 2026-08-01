1fd760a35523eb1ee81abbef7508a029324d4a10
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-01 14:39:23 +0530

    Add files via upload

---
+++ Truck Tour/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'truckTour' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+     */
+
+    public static int truckTour(List<List<int>> petrolpumps)
+    {
+        int start = 0;
+        int balance = 0;
+        int deficit = 0;
+
+        for (int i = 0; i < petrolpumps.Count; i++)
+        {
+            balance += petrolpumps[i][0] - petrolpumps[i][1];
+
+            if (balance < 0)
+            {
+                start = i + 1;
+                deficit += balance;
+                balance = 0;
+            }
+        }
+
+        return (balance + deficit >= 0) ? start : -1;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = Console.Out;
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> petrolpumps = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            petrolpumps.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(petrolpumpsTemp => Convert.ToInt32(petrolpumpsTemp)).ToList());
+        }
+
+        int result = Result.truckTour(petrolpumps);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
