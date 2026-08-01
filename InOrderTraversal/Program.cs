b83c8b2b66f0f7d5a1fca77840ba2a458fd27d97
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-01 12:22:58 +0530

    Add files via upload

---
+++ InOrderTraversal/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node left, right;
+
+    public Node(int value)
+    {
+        data = value;
+        left = right = null;
+    }
+}
+
+class Solution
+{
+    // Insert into BST
+    static Node Insert(Node root, int data)
+    {
+        if (root == null)
+            return new Node(data);
+
+        if (data <= root.data)
+            root.left = Insert(root.left, data);
+        else
+            root.right = Insert(root.right, data);
+
+        return root;
+    }
+
+    // Inorder Traversal
+    static void inOrder(Node root)
+    {
+        if (root == null)
+            return;
+
+        inOrder(root.left);
+        Console.Write(root.data + " ");
+        inOrder(root.right);
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] values = Console.ReadLine().Split(' ');
+
+        Node root = null;
+
+        for (int i = 0; i < n; i++)
+        {
+            root = Insert(root, int.Parse(values[i]));
+        }
+
+        inOrder(root);
+    }
+}
