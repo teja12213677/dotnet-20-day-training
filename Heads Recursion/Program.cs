ba7bb90b9c82142cc929cf5678706ea15f90480a
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-07 17:01:10 +0530

    Add files via upload

---
+++ Heads Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    // Head Recursive Method
+    static void SumDigitsReversed(int n)
+    {
+        // Base Case
+        if (n == 0)
+            return;
+
+        // Recursive Call (Head Recursion)
+        SumDigitsReversed(n / 10);
+
+        // Processing after recursion
+        Console.Write(n % 10 + " ");
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Digits in reverse order: ");
+        SumDigitsReversed(number);
+
+        Console.ReadLine();
+    }
+}
