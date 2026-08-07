1b32eb6c423136e51bb2f39fe178384fbf68f3bf
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-07 12:17:13 +0530

    Add files via upload

---
+++ Tails Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+  
+    static int Factorial(int n, int accumulator = 1)
+    {
+      
+        if (n == 0 || n == 1)
+            return accumulator;
+
+        return Factorial(n - 1, accumulator * n);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int result = Factorial(number);
+
+        Console.WriteLine("Factorial = " + result);
+    }
+}
