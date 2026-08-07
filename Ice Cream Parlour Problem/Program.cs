5cb6e9bd4fdf3321de2286d3ab8b45bbbf38ad77
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-07 17:11:21 +0530

    Add files via upload

---
+++ Ice Cream Parlour Problem/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<int> icecreamParlor(int m, List<int> arr)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            int complement = m - arr[i];
+
+            if (map.ContainsKey(complement))
+            {
+                return new List<int> { map[complement] + 1, i + 1 };
+            }
+
+            if (!map.ContainsKey(arr[i]))
+            {
+                map[arr[i]] = i;
+            }
+        }
+
+        return new List<int>();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = Console.Out;
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine()
+                                   .TrimEnd()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToList();
+
+            List<int> result = Result.icecreamParlor(m, arr);
+
+            textWriter.WriteLine(String.Join(" ", result));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
