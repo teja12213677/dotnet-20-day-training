fe62c3d8e1d1799f14b17ef6e56cc400761411ef
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-07 17:04:55 +0530

    Add files via upload

---
+++ Trees Recursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+
+    static int CountPaths(int rows, int cols)
+    {
+ 
+        if (rows == 1 || cols == 1)
+            return 1;
+
+        return CountPaths(rows - 1, cols) +
+               CountPaths(rows, cols - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter number of columns: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        int totalPaths = CountPaths(rows, cols);
+
+        Console.WriteLine("Total Paths = " + totalPaths);
+    }
+}
