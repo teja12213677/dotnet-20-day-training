2e159578b1e35133a35b45a6dae93f181689f4bf
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-07 12:11:24 +0530

    Add files via upload

---
+++ Indirect Recursion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPositiveChain(int n)
+    {
+        if (n == 0)
+            return true;
+
+        if (n > 0)
+            return IsNegativeChain(n - 1);
+        else
+            return IsNegativeChain(n + 1);
+    }
+
+    static bool IsNegativeChain(int n)
+    {
+        if (n == 0)
+            return true;
+
+        if (n > 0)
+            return IsPositiveChain(n - 1);
+        else
+            return IsPositiveChain(n + 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool result;
+
+        if (number >= 0)
+            result = IsPositiveChain(number);
+        else
+            result = IsNegativeChain(number);
+
+        Console.WriteLine(result);
+    }
+}
