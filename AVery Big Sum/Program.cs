dd1eb62090ab6eaf43de82df75f8fc62597a14c3
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-08 10:54:56 +0530

    Add files via upload

---
+++ AVery Big Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static long aVeryBigSum(List<long> ar)
+    {
+        long sum = 0;
+
+        foreach (long number in ar)
+        {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string[] input = Console.ReadLine().Split(' ');
+
+        List<long> numbers = new List<long>();
+
+        foreach (string value in input)
+        {
+            numbers.Add(Convert.ToInt64(value));
+        }
+
+        long result = aVeryBigSum(numbers);
+
+        Console.WriteLine(result);
+    }
+}
