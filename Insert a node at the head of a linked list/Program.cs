c325d273d6a9715e80c75d49f30e6f93458e10e6
Teja Udatala <119668820+teja12213677@users.noreply.github.com>
2026-08-10 10:56:52 +0530

    Add files via upload

---
+++ Insert a node at the head of a linked list/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Node
+{
+    public int Data;
+    public Node Next;
+
+    public Node(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+class LinkedList
+{
+    Node head;
+
+    public void InsertAtHead(int data)
+    {
+        Node newNode = new Node(data);
+        newNode.Next = head;
+
+        head = newNode;
+    }
+
+    public void Display()
+    {
+        Node current = head;
+
+        while (current != null)
+        {
+            Console.Write(current.Data + " -> ");
+            current = current.Next;
+        }
+
+        Console.WriteLine("NULL");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList list = new LinkedList();
+
+        list.InsertAtHead(30);
+        list.InsertAtHead(20);
+        list.InsertAtHead(10);
+
+        list.Display();
+    }
+}
